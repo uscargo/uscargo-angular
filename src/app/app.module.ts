6b1bff8037dc958d34e82b1a7bee73e050b25cfc
Ted-Koomen <koomen.ted@gmail.com>
2017-10-25 15:21:42 -0400

    add sign up component and fix route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule} from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { NotfoundComponent } from './notfound/notfound.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    SigninComponent,
+    SignupComponent,
+    NotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent},
+      { path: 'sign-in', component: SigninComponent},
+      { path: 'sign-up', component: SignupComponent},
+      { path: '**', component: NotfoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
